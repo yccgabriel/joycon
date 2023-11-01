79759a4a4c256b621b4489f454f3bd8a35857cf0
Gabriel Y C Chan <isoleucine.local>
2023-11-01 13:55:44 +0800

    handle key-combo but not every user-friendly.

--- joycon.py
+++ joycon.py
@@ -7,6 +7,7 @@ from morse import decode
 
 try:
     joycon_L, joycon_R = ButtonEventJoyCon(*get_L_id()), ButtonEventJoyCon(*get_R_id())
+    # joycon_L = ButtonEventJoyCon(*get_L_id())
 except ValueError:
     print('check joycon connected? exit.')
     exit(1)
@@ -14,29 +15,46 @@ except ValueError:
 sigint_event = threading.Event()    # https://stackoverflow.com/a/71420261
 loop = asyncio.get_event_loop()
 queue = asyncio.Queue()
+q2 = asyncio.Queue()
 
 def joycon_listener():
     while not sigint_event.is_set():
         for event_type, status in chain(joycon_L.events(), joycon_R.events()):
+        # for event_type, status in chain(joycon_L.events()):
             asyncio.run_coroutine_threadsafe(queue.put((event_type, status)), loop)     # https://stackoverflow.com/a/43275001
 
 async def main_loop():
+    buf = {'v': [], '^': []}
+    while not sigint_event.is_set():
+        key, direction = await queue.get()
+        if direction == 1:
+            buf['v'].append(key)
+        elif direction == 0:
+            buf['v'].remove(key)
+            buf['^'].append(key)
+        else:
+            raise ValueError(f'unexpected direction: {direction}')
+        if len(buf['v']) == 0:
+            q2.put_nowait(set(buf['^']))
+            buf['^'].clear()
+
+async def pain_loop():
     keyboard = Controller()
     buf = ''
     while not sigint_event.is_set():
-        key_event = await queue.get()
-        if key_event in [('r',1), ('l',1)]:
+        keys = await q2.get()
+        if keys in [{'l'}, {'r'}]:
             buf += '.'
-        if key_event in [('zr',1), ('zl', 1)]:
+        if keys in [{'zl'}, {'zr'}]:
             buf += '-'
-        if key_event in [('x',1)]:
+        if keys == {'l', 'r', 'zl', 'zr'} or keys == {'x'}:
             keyboard.type(decode(buf))
             buf = ''
-        if key_event in [('a',1)]:
+        if keys == {'a'}:
             keyboard.press(Key.enter)
             keyboard.release(Key.enter)
             buf = ''
-        if key_event in [('b',1)]:
+        if keys == {'b'}:
             keyboard.press(Key.backspace)
             keyboard.release(Key.backspace)
             buf = ''
@@ -45,6 +63,7 @@ async def main():
     await asyncio.gather(
         asyncio.to_thread(joycon_listener),
         main_loop(),
+        pain_loop(),
     )
 
 async def cleanup():
